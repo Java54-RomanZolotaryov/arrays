7cf2837058da41973420e9555c912c2305372208
Roman <romanzolotdev@gmail.com>
2024-04-02 18:42:34 +0300

    added search method

--- src/telran/util/Arrays.java
+++ src/telran/util/Arrays.java
@@ -57,15 +57,19 @@ public class Arrays {
 		return left > right ? -(left + 1) : middle;
 	}
 	
-	public static <T> T[] removeIf(T[] array, Predicate<T> predicate) {
+	public static <T> T[] search(T[] array, Predicate<T> predicate) {
 		T[] result = java.util.Arrays.copyOf(array, array.length);
 		int insertIndex = 0;
 		for (int i = 0; i < array.length; i++) {
-			if (!predicate.test(array[i])) {
+			if (predicate.test(array[i])) {
 				result[insertIndex++] = array[i];
 			}
 		}
 		return java.util.Arrays.copyOf(result, insertIndex);
+	}
+	
+	public static <T> T[] removeIf(T[] array, Predicate<T> predicate) {
+		return search(array, predicate.negate());
 	}
 
 	private static <T> void swap(T[] array, int i, int j) {
